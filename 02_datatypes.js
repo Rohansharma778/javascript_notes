c99d7c3c2ef8129a7eacea3925d00e8d8cf6417d
Rohansharma778 <159758258+Rohansharma778@users.noreply.github.com>
2024-08-28 12:45:56 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+let score="33abc"
+// 33 is a number but 33abc in (NaN) Not a number
+
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInnumber=Number(score)
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+let IsloggedIn=1
+//1=true,0=false,""(empty string) is false, "rohan"=true
+let x=Boolean(IsloggedIn)
+console.log(x);
+
+let number ="33"
+
+let xr=String(number)
+console.log(xr);
+
+
+let value=true
+ 
+let value1=Number(value)
+console.log(typeof value1);
