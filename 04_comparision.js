c968aa0d01e8e44302343b068c2ca4c34451aa46
Rohansharma778 <159758258+Rohansharma778@users.noreply.github.com>
2024-08-30 19:20:31 +0530

    Add files via upload

---
+++ 04_comparision.js
@@ -0,0 +1,5 @@
+console.log(null>0);//when comparision  null becomes zero or a number so now null=0then is 0>0 no so false
+console.log(null==0);//when == equality comes null becomes the string  valus is always 1 so 1==0 so false
+console.log(null>=0);//here null is compared first so it is considered as 0 so is 0 greater of equal to 0 (not greater but equal to 0) so true it equal 0=0
+console.log("2"===2);//strict checks the datatypes as well
+
