78fa721d2977ec2d8a82be4bb91025c9eea3f720
Rohansharma778 <159758258+Rohansharma778@users.noreply.github.com>
2024-12-23 19:44:07 +0530

    Add files via upload

---
+++ checked property/checkedproperty.js
@@ -0,0 +1,28 @@
+
+const mycheckbox=document.getElementById("mycheckbox");
+const visabtn=document.getElementById("visabtn");
+const mastercardbtn=document.getElementById("mastercardbtn");
+const paypalbtn=document.getElementById("paypalbtn");
+const subresult=document.getElementById("subresult");
+const paymentresult=document.getElementById("paymentresult");
+
+mysubmit.onclick=function(){
+    if(mycheckbox.checked){
+        subresult.textContent="you are subscribed";
+    }
+    else{
+        subresult=textContent="you are not subscribed";
+    }
+    if(visabtn.checked){
+        paymentresult.textContent="you are paying with visa";
+    }
+    else if(mastercardbtn.checked){
+        paymentresult.textContent="you are paying with master card";
+    }
+    else if(paypalbtn.checked){
+        paymentresult.textContent="you are paying with paypal card";
+    }
+    else{
+        paymentresult.textContent="you have to select a payment method"
+    }
+}
